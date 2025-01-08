921f6be29713a04a8ceb0fd0736c3d146130f2ee
Ben Hofer <ben.hofer@gmail.com>
2025-01-08 16:16:03 -0500

    initial steps toward replacing content

--- src/App.js
+++ src/App.js
@@ -1,129 +1,156 @@
-import React, {Fragment, useState, useEffect} from 'react';
-import Search from './pages/Search';
-import Meditations from './pages/Meditations';
+import React, { useState } from "react";
+import Search from "./pages/Search";
+import Meditations from "./pages/Meditations";
 import BrowseLinks from "./components/BrowseLinks";
-import styles from './assets/css/pages/app.module.css';
-import './assets/css/global/main.css';
-import './assets/css/global/typography.css';
-import './assets/css/global/colors.css';
-import './assets/css/global/grid.css';
-import './assets/css/utilities/util.css';
-import './assets/css/objects/objects.css';
-import Gels from './components/Gels';
-import {
-  TransitionGroup,
-  CSSTransition
-} from "react-transition-group";
+import styles from "./assets/css/pages/app.module.css";
+import "./assets/css/global/main.css";
+import "./assets/css/global/typography.css";
+import "./assets/css/global/colors.css";
+import "./assets/css/global/grid.css";
+import "./assets/css/utilities/util.css";
+import "./assets/css/objects/objects.css";
+import Gels from "./components/Gels";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import {
   BrowserRouter as Router,
-  Switch, Redirect,
-  Route, useLocation, NavLink
+  Switch,
+  Redirect,
+  Route,
+  useLocation,
 } from "react-router-dom";
-import cross from './assets/img/cross.svg';
+import cross from "./assets/img/cross.svg";
 
 const browseLinks = [
   {
-    id: 'author',
-    name: 'Author'
-  },{
-    id: 'collection',
-    name: 'Collection'
-  },{
-    id: 'work',
-    name: 'Work'
+    id: "author",
+    name: "Author",
   },
-]
+  {
+    id: "collection",
+    name: "Collection",
+  },
+  {
+    id: "work",
+    name: "Work",
+  },
+];
 
 export default function App() {
   return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/app" />
-          </Route>
-          <Route path="*">
-            <AnimationApp />
-          </Route>
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/app' />
+        </Route>
+        <Route path='*'>
+          <AnimationApp />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-function AnimationApp() { 
-  const [ engagement, setEngagement ] = useState('welcome');
-  const [ colorClass, setColorClass ] = useState('gold');
-  const [ bgImg, setBgImg ] = useState({src: cross, alt: 'The Cross'});
-  const [ showCurious, setShowCurious ] = useState(false);
+function AnimationApp() {
+  const [engagement, setEngagement] = useState("welcome");
+  const [colorClass, setColorClass] = useState("gold");
+  const [bgImg, setBgImg] = useState({ src: cross, alt: "The Cross" });
+  const [showCurious, setShowCurious] = useState(false);
 
   let location = useLocation();
 
   function goHome() {
-    setBgImg({src: cross, alt: 'The Cross'});
-    setEngagement('welcome');
-    setColorClass('gold');
+    setBgImg({ src: cross, alt: "The Cross" });
+    setEngagement("welcome");
+    setColorClass("gold");
   }
 
   return (
-      <div>
-          <TransitionGroup>
-            {/*
+    <div>
+      <TransitionGroup>
+        {/*
               This is no different than other usage of
               <CSSTransition>, just make sure to pass
               `location` to `Switch` so it can match
               the old location as it animates out.
             */}
-              <CSSTransition
-                key={location.key}
-                classNames="fade"
-                timeout={300}
-              >
-                <Gels color={colorClass} engagement={engagement} img={bgImg}/>
-              </CSSTransition>
-          </TransitionGroup>
+        <CSSTransition key={location.key} classNames='fade' timeout={300}>
+          <Gels color={colorClass} engagement={engagement} img={bgImg} />
+        </CSSTransition>
+      </TransitionGroup>
 
-          <div id="main" className={`main`}>
-            {showCurious && 
-              <div className={styles.curious} onClick={()=>setShowCurious(false)}>
-                <div>
-                  <h1>About Tempora</h1>
-                  <p>Tempora provides resources for the practice of centering prayer.</p>
-                  <p>"Centering Prayer is a receptive method of Christian silent prayer that prepares us to receive the gift of contemplative prayer, prayer in which we experience God’s presence within us, closer than breathing, closer than thinking, closer than consciousness itself."</p>
-                  <p>&mdash; <a target="_blank" rel="noopener noreferrer" href="https://www.contemplativeoutreach.org/centering-prayer-method/">Learn more at ContemplativeOutreach.org</a></p>
+      <div id='main' className={`main`}>
+        {showCurious && (
+          <div className={styles.curious} onClick={() => setShowCurious(false)}>
+            <div>
+              <h1>About Tempora</h1>
+              <p>
+                Tempora provides resources for the practice of centering prayer.
+              </p>
+              <p>
+                "Centering Prayer is a receptive method of Christian silent
+                prayer that prepares us to receive the gift of contemplative
+                prayer, prayer in which we experience God’s presence within us,
+                closer than breathing, closer than thinking, closer than
+                consciousness itself."
+              </p>
+              <p>
+                &mdash;{" "}
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://www.contemplativeoutreach.org/centering-prayer-method/'
+                >
+                  Learn more at ContemplativeOutreach.org
+                </a>
+              </p>
+            </div>
+          </div>
+        )}
+        <Router>
+          <Switch>
+            <Route
+              path='/app/:m'
+              children={
+                <Meditations
+                  browseLinks={browseLinks}
+                  goHome={goHome}
+                  setColor={(c) => setColorClass(c)}
+                  setEngagement={setEngagement}
+                />
+              }
+            />
+            <Route exact path='/app'>
+              <div>
+                <header className={`${styles.header} app-header home`}>
+                  <div onClick={() => setShowCurious(true)}>
+                    <span className={`${styles.subheader}`}>The Music of</span>
+                    <h1>BENJAMIN HOFER</h1>
+                  </div>
+                </header>
+                <Search />
+                <div className={styles.browse_section}>
+                  <BrowseLinks
+                    links={browseLinks}
+                    setShowCurious={() => setShowCurious(true)}
+                    heading={true}
+                  />
                 </div>
               </div>
-            }
-            <Router>
-              <Switch>     
-                <Route path="/app/:m" children={<Meditations browseLinks={browseLinks} goHome={goHome} setColor={(c) => setColorClass(c)} setEngagement={setEngagement} />} />
-                <Route exact path="/app">
-                  <div>
-                    <header className={`${styles.header} app-header home`}> 
-                      <div>
-                        <h1>TEMPORA</h1>
-                        <span className={`${styles.subtitle} subtitle2`} onClick={() => setShowCurious(true)}>
-                          An App for Centering Prayer
-                        </span>
-                      </div>
-                    </header>  
-                    <Search />
-                    <div className={styles.browse_section}>
-                      <BrowseLinks links={browseLinks} heading={true} />
-                    </div>
-                  </div> 
-                </Route>
-              </Switch>
-                      
-            </Router>
-            
-            <div className="push"></div>
+            </Route>
+          </Switch>
+        </Router>
+
+        <div className='push'></div>
+      </div>
+
+      <footer className={`${styles.footer} page-footer`}>
+        <div className='lower-footer'>
+          <div className='u-text-center'>
+            Let everything that has breath praise the LORD! Praise the LORD!{" "}
+            <i style={{ display: "block" }}>Psalm 150:6</i>
           </div>
-
-            <footer className={`${styles.footer} page-footer`}>
-              <div className="lower-footer">
-                <div className="u-text-center">O God, you will keep in perfect peace those whose minds are fixed on you. <i>Isaiah 26:3</i></div>
-              </div>
-            </footer>
-
+        </div>
+      </footer>
     </div>
   );
 }
--- src/components/BrowseLinks.js
+++ src/components/BrowseLinks.js
@@ -1,27 +1,42 @@
-import React, {useState} from 'react';
-import styles from '../assets/css/components/browselinks.module.css';
+import React, { useState } from "react";
+import styles from "../assets/css/components/browselinks.module.css";
 import { NavLink } from "react-router-dom";
 
 function BrowseLinks(props) {
-
-    return (
-        <div className="browse-links" style={{flexWrap: 'wrap'}}>
-                { props.heading &&
-                    <h1 className={styles.browse_heading}>
-                        Browse by
-                    </h1>
-                }  
-            <ul className={styles.browse_links}>
-              
-                {props.links.map(link => 
-                    <li key={'browse-by-'+link.id+'-link'} >
-                        <NavLink className={styles.browse_link} to={"/app/browse?filter="+link.id}>{link.name}</NavLink>
-                    </li>
-                )}
-            </ul>
-        </div>
-    )
-
+  return (
+    <div className='browse-links' style={{ flexWrap: "wrap" }}>
+      <ul className={styles.browse_links}>
+        <li>
+          <span
+            className={`${styles.browse_link} subtitle2`}
+            onClick={() => props.setShowCurious()}
+          >
+            About
+          </span>
+        </li>{" "}
+        <li>
+          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
+            Shows
+          </NavLink>
+        </li>{" "}
+        <li>
+          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
+            Blog
+          </NavLink>
+        </li>{" "}
+        <li>
+          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
+            Videos
+          </NavLink>
+        </li>{" "}
+        <li>
+          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
+            Support
+          </NavLink>
+        </li>{" "}
+      </ul>
+    </div>
+  );
 }
 
-export default BrowseLinks; 
+export default BrowseLinks;
