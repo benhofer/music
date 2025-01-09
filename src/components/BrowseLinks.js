145fa2bc06607b67fcfd3d621eba0554c9b18be6
Ben Hofer <ben.hofer@gmail.com>
2025-01-08 22:10:49 -0500

    added links

--- src/components/BrowseLinks.js
+++ src/components/BrowseLinks.js
@@ -20,19 +20,54 @@ function BrowseLinks(props) {
           </NavLink>
         </li>{" "}
         <li>
-          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
-            Blog
-          </NavLink>
+          <a
+            className={styles.browse_link}
+            href='https://www.patreon.com/benjaminhofer'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Patreon
+          </a>
         </li>{" "}
         <li>
-          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
+          <a
+            className={styles.browse_link}
+            href='https://benhofer.bandcamp.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Music
+          </a>
+        </li>{" "}
+        <li>
+          <a
+            className={styles.browse_link}
+            href='https://www.instagram.com/benjaminhofer'
+            target='_blank'
+            rel='noreferrer'
+          >
             Videos
-          </NavLink>
+          </a>
         </li>{" "}
         <li>
-          <NavLink className={styles.browse_link} to={"/app/browse?filter="}>
-            Support
-          </NavLink>
+          <a
+            className={styles.browse_link}
+            href='https://www.themusicofbenjaminhofer.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Sign Up
+          </a>
+        </li>{" "}
+        <li>
+          <a
+            className={styles.browse_link}
+            href='https://us11.list-manage.com/contact-form?u=6a351b2bd64ec3d2dea2428f3&form_id=7755296797aecc08770d09e7fcc41727'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Contact
+          </a>
         </li>{" "}
       </ul>
     </div>
