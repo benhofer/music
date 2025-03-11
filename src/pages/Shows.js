702ca59863743ac2a938a9679c657d67aebd5aa5
Ben Hofer <ben.hofer@gmail.com>
2025-03-11 14:35:43 -0400

    added shows table with content

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -1,4 +1,10 @@
 import React, { useEffect } from "react";
+import "../assets/css/global/main.css";
+import "../assets/css/global/typography.css";
+import "../assets/css/global/colors.css";
+import "../assets/css/global/grid.css";
+import "../assets/css/utilities/util.css";
+import "../assets/css/objects/objects.css";
 
 function Shows(props) {
   const location = document.location.href;
@@ -11,8 +17,41 @@ function Shows(props) {
   }, [props, location]);
 
   return (
-    <div class='content'>
-      <p>No upcoming shows at this time. Please check back soon!</p>
+    <div className='content'>
+      <h2>Upcoming Shows</h2>
+      {/*<p>No upcoming shows at this time. Please check back soon!</p>*/}
+      {/* <ul className={styles.showsList}> */}
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Venue</th>
+            <th>Location</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>the Bull</td>
+            <td>18 SW 1st Ave, Gainesville, FL 32601</td>
+            <td>Saturday, March 22</td>
+            <td>8:00pm</td>
+          </tr>
+
+          <tr>
+            <td>One Love Cafe (Farmer's Market)</td>
+            <td>4989 Northwest 40th Place, Gainesville, FL 32606</td>
+            <td>Tuesday, May 27</td>
+            <td>4 - 6pm</td>
+          </tr>
+          <tr>
+            <td>One Love Cafe (Brunch)</td>
+            <td>4989 Northwest 40th Place, Gainesville, FL 32606</td>
+            <td>Saturday, June 7</td>
+            <td>12 - 3pm</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
